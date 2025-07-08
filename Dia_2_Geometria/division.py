385016fa468fcf69303e7ff9d7c5381ab15d773c
Gino Ruben Giorgi <ginorubengiorgi@gmail.com>
2025-07-08 15:45:33 -0300

    division

--- Dia_2_Geometria/division.py
+++ Dia_2_Geometria/division.py
@@ -2,13 +2,12 @@ n = int(input())
 
 for _ in range(n):
     tupla = list(map(int, input().split()))
+    a, b = tupla[0], tupla[1]
     divisible = 1
-    xi = tupla[0]
-    while tupla[0]%divisible == 0:
+    while a % divisible == 0:
         divisible += 1
     divisible -= 1
-    while xi%tupla[1] == 0 or tupla[0]%xi != 0:
+    xi = a
+    while xi % b == 0 or a % xi != 0:
         xi -= divisible
     print(xi)
-         
-
