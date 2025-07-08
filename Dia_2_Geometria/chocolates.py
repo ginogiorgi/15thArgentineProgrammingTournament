23d9dcb7fb1340c2a9414766c1b21e8e998720ab
Juan Manuel Garcia <juanmanuel_garcia98@hotmail.com>
2025-07-08 16:26:14 -0300

    Update chocolates.py

--- Dia_2_Geometria/chocolates.py
+++ Dia_2_Geometria/chocolates.py
@@ -3,7 +3,7 @@ arr = list(map(int, input().split()))
 result = arr[-1]
 totalResult = 0
 for chocolates in range(n-2, -1, -1):
-    if arr[chocolates] < arr[chocolates+1] or arr[chocolates+1] == 1:
+    if arr[chocolates] < arr[chocolates+1] or arr[chocolates+1] == 1 and arr[chocolates] != 1:
         result += arr[chocolates]
     else:
         result += arr[chocolates+1]-1
