4a866c19c327c0d19de4cc8b2a57a65c1938167f
Juan Manuel Garcia <juanmanuel_garcia98@hotmail.com>
2025-07-08 16:22:48 -0300

    Create chocolates.py

---
+++ Dia_2_Geometria/chocolates.py
@@ -0,0 +1,19 @@
+n = int(input())
+arr = list(map(int, input().split()))
+result = arr[-1]
+totalResult = 0
+for chocolates in range(n-2, -1, -1):
+    if arr[chocolates] < arr[chocolates+1] or arr[chocolates+1] == 1:
+        result += arr[chocolates]
+    else:
+        result += arr[chocolates+1]-1
+    if result > totalResult:
+        totalResult = result
+    if arr[chocolates] == 1:
+        result = 0
+    
+        
+print(totalResult)
+    
+    
+    
