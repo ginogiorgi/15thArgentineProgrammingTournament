3c5033e12d91bf594015bdc3e43020ca2fcf786b
Juan Manuel Garcia <juanmanuel_garcia98@hotmail.com>
2025-07-08 18:08:21 -0300

    Create linear_keyboard.py

---
+++ Dia_2_Geometria/linear_keyboard.py
@@ -0,0 +1,12 @@
+t = int(input())
+
+for _ in range(t):
+    keyboard = str(input())
+    word = str(input())
+    totalTime = 0
+    if len(word) == 1:
+        print(0)
+    for i in range(len(word)-1):
+        totalTime += abs(keyboard.find(word[i]) - keyboard.find(word[i+1]))
+    print(totalTime)
+        
