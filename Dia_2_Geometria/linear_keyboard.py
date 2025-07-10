3519943697e87e4e72dd784ad3b8895a722c8650
Gino Ruben Giorgi <ginorubengiorgi@gmail.com>
2025-07-10 10:10:19 -0300

    Fix Dia 2

--- Dia_2_Geometria/linear_keyboard.py
+++ Dia_2_Geometria/linear_keyboard.py
@@ -6,7 +6,7 @@ for _ in range(t):
     totalTime = 0
     if len(word) == 1:
         print(0)
-    for i in range(len(word)-1):
-        totalTime += abs(keyboard.find(word[i]) - keyboard.find(word[i+1]))
-    print(totalTime)
-        
+    else:
+        for i in range(len(word) - 1):
+            totalTime += abs(keyboard.find(word[i]) - keyboard.find(word[i + 1]))
+        print(totalTime)
